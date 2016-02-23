a6bea57506a4bd50d40bc22f86d5f5594c76f748
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2016-02-23 17:20:32 -0500

    Made client prettier
    awwww

--- Client/src/io/winterdev/client/ui/ScreenAlert.java
+++ Client/src/io/winterdev/client/ui/ScreenAlert.java
@@ -6,18 +6,23 @@
 package io.winterdev.client.ui;
 
 import io.winterdev.client.Client;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
  *
  * @author frede
  */
-public class ScreenAlert extends StackPane{
+public class ScreenAlert extends VBox{
     public ScreenAlert(Client client,int id,String title,String type){
         
         Stage stage =  new Stage();
@@ -31,21 +36,52 @@ public class ScreenAlert extends StackPane{
        this.getStyleClass().add(type.equalsIgnoreCase("alert")?"alert":"notification");
         Label label = new Label(title);
         label.getStyleClass().add("alertLabel");
+        VBox.setVgrow(this, Priority.ALWAYS);
+        label.setPrefWidth(Double.MAX_VALUE);
         this.getChildren().add(label);
         scene.setOnKeyPressed((KeyEvent ke)->{
             System.out.println(ke.getCode());
-            if(ke.getCode() == KeyCode.A){
-                client.post(id);
-                stage.close();
-            }else if(ke.getCode() == KeyCode.R){
-                client.remove(id);
-                stage.close();
-            }else if(ke.getCode() == KeyCode.S){
-                client.spoil(id);
-                stage.close();
+            if(null != ke.getCode())switch (ke.getCode()) {
+                case A:
+                    client.post(id);
+                    stage.close();
+                    break;
+                case R:
+                    client.remove(id);
+                    stage.close();
+                    break;
+                case S:
+                    client.spoil(id);
+                    stage.close();
+                    break;
+                default:
+                    break;
             }
         });
+        HBox options = new HBox();
+        options.getStyleClass().add("alertOptions");
         
+        Button post = new Button("Accept");
+        post.setOnAction((ActionEvent e)->{
+            client.post(id);
+            stage.close();
+        });
+        post.getStyleClass().add("alertAccept");
+        Button remove = new Button("Remove");
+        remove.setOnAction((ActionEvent e)->{
+            client.remove(id);
+            stage.close();
+        });
+        remove.getStyleClass().add("alertReject");
+        Button spoil = new Button("Spoil");
+        spoil.setOnAction((ActionEvent e)->{
+            client.spoil(id);
+            stage.close();
+        });
+        spoil.getStyleClass().add("alertSpoil");
+        
+        options.getChildren().addAll(post,remove,spoil);
+        this.getChildren().add(options);
         
     }
 }
